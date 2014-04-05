4b4de0fe619cd5d88aba7e93b989ed2a2c9e18e1
Xmister <xmisterhu@gmail.com>
2014-04-05 10:52:25 +0200

    Add install script
    Signed-off-by: Xmister <xmisterhu@gmail.com>

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ADB=/home/xmister/Downloads/android-sdk-linux/platform-tools/adb
+${ADB} disconnect
+${ADB} kill-server
+${ADB} connect cubie
+${ADB} shell mount -o remount,rw /system
+cd /home/xmister/Cubie
+${ADB} push build/android /system/vendor/modules
+${ADB} shell chmod 0755 /system/vendor/modules/*
+${ADB} shell mount -o remount,ro /system
+${ADB} push packing/boot.img /cache/boot.img
+${ADB} shell dd if=/cache/boot.img of=/dev/block/nandc
